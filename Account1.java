2f439fba4f72130ba7b079b386a255113952b777
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-04-09 16:57:32 +0400

    Update Account1.java

--- Account1.java
+++ Account1.java
@@ -1,43 +1,40 @@
 import java.util.Scanner;
 
 public class Account1 {
-    private double balance;  
-    
-    
-    public Account1() {
-        balance = 1520.0;
+  private double balance;
+
+  public Account1() {
+    balance = 0.0;
+  }
+
+  public Account1(double Balance) {
+    this.balance = Balance;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public void setBalance(double newBalance) {
+    this.balance = newBalance;
+  }
+
+  public void deposit(double amount) throws Exception {
+    if (amount > 0) {
+      balance = balance + amount;
+      System.out.println("Deposit of " + amount + " successful");
+    } else {
+      throw new Exception("Invalid amount for deposit");
     }
+  }
 
-    public Account1 (double Balance) {
-        this.balance = Balance;
+  public void withDraw(double amount) throws Exception {
+    if (amount > 0 && amount <= balance) {
+      balance = balance - amount;
+      System.out.println("Withdrawal of " + amount + " successful");
+    } else {
+      throw new Exception("Insufficient funds or invalid amount for withdrawal");
     }
+  }
 
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double newBalance) {
-       this.balance = newBalance;
-    }
-
-    public void deposit(double amount) throws Exception{
-        if (amount > 0) {
-            balance = balance + amount;
-            System.out.println("Deposit of " + amount + " successful");
-        } else {
-           throw new Exception("Invalid amount for deposit");
-        }
-    }
-
-    public void withDraw(double amount) throws Exception{
-        if (amount > 0 && amount <= balance) {
-            balance = balance - amount;
-            System.out.println("Withdrawal of " + amount + " successful");
-        } else {
-            throw new Exception("Insufficient funds or invalid amount for withdrawal");
-        }
-    }
-
-   
- }
-
+}
